a06c1f1c07a1b44fd3ce2e84eac3f1525e552255
deshpank <deshpank@purdue.edu>
2024-10-29 15:04:28 -0400

    Create Login.java
    Signed-off-by: deshpank <deshpank@purdue.edu>

---
+++ Login.java
@@ -0,0 +1,13 @@
+public class Login {
+    public static String username;
+    public static String password;
+    public static boolean isValid;
+    public static boolean existingUser;
+
+    public Login(String username, String password, boolean isValid) {
+        this.username = username;
+        this.password = password;
+        this.isValid = false;
+        this.isValid = existingUser = false;
+    }
+}
