4b469db3ec566ce7e74cdc775ee6c63f55d40021
deshpank <deshpank@purdue.edu>
2024-10-29 15:45:40 -0400

    Update Login.java
    Signed-off-by: deshpank <deshpank@purdue.edu>

--- Login.java
+++ Login.java
@@ -1,4 +1,4 @@
-public class Login extends UserProfile{
+public class Login implements UserProfileInterface {
     public static String username;
     public static String password;
     public static boolean isValid;
