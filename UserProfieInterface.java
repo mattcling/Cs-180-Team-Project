88c1539209af8332ab1319a647b948766509079c
CharlotteFalus <cfalus@purdue.edu>
2024-10-29 14:38:49 -0400

    Update and rename UserInterface.java to UserProfieInterface.java
    makeing user profile interface 
    
    Signed-off-by: CharlotteFalus <cfalus@purdue.edu>

---
+++ UserProfieInterface.java
@@ -0,0 +1,24 @@
+/**
+ * Group Project
+ *
+ * This interface contains user profile details
+ *
+ * @author Charlotte Falus, LO4
+ *
+ * @version October 29, 2024
+ *
+ */
+public interface UserProfileInterface {
+  //getters
+  public String getUsername();
+  public String getPassword();
+  public ArrayList<User> getFriends();
+  public String toString();
+  //setters
+  public void setUsername(String username);
+  public void setPassword(String password);
+  public void setFriends(ArrayList<User> friends);
+  //friend methods
+  public void addFriend(String friend);
+  public void deleteFriend(String friend);
+}
