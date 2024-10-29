c9cd030d00d910ec2aa340038f7ca4dd86dde202
CharlotteFalus <cfalus@purdue.edu>
2024-10-29 15:16:44 -0400

    Update UserProfileInterface.java
    added blocked stuff
    
    Signed-off-by: CharlotteFalus <cfalus@purdue.edu>

--- UserProfileInterface.java
+++ UserProfileInterface.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
-
 /**
  * Group Project
  *
  * This interface contains user profile details for user class
  *
- * @author Charlotte Falus, LO4
+ * @author Group, LO4
  *
  * @version October 29, 2024
  *
@@ -14,13 +12,15 @@ public interface UserProfileInterface {
   //getters
   public String getUsername();
   public String getPassword();
-  public ArrayList<UserProfile> getFriends();
+  public ArrayList<User> getFriends();
+  public ArrayList<User> getBlocked();
   public String toString();
   //setters
   public void setUsername(String username);
   public void setPassword(String password);
-  public void setFriends(ArrayList<UserProfile> friends);
+  public void setFriends(ArrayList<User> friends);
   //friend methods
   public void addFriend(String friend);
   public void deleteFriend(String friend);
+  public void block(User user);
 }
