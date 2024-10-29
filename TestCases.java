9c34964d30ed7c25e161754ab5dbf94c22e05326
CharlotteFalus <cfalus@purdue.edu>
2024-10-29 15:27:44 -0400

    Create TestCases.java
    created the file and put in the outline/ex of a test case the directions gave us
    
    Signed-off-by: CharlotteFalus <cfalus@purdue.edu>

---
+++ TestCases.java
@@ -0,0 +1,23 @@
+// @Test(timeout = 1000)
+// public void testExpectedOne() {
+//     // Set the input        
+//     // Separate each input with a newline (\n). 
+//     String input = "Line One\nLine Two\n"; 
+
+//     // Pair the input with the expected result
+//     String expected = "Insert the expected output here" 
+
+//     // Runs the program with the input values
+//     // Replace TestProgram with the name of the class with the main method
+//     receiveInput(input);
+//     TestProgram.main(new String[0]);
+
+//     // Retrieves the output from the program
+//     String stuOut = getOutput();
+
+//     // Trims the output and verifies it is correct. 
+//     stuOut = stuOut.replace("\r\n", "\n");
+//     assertEquals("Error message if output is incorrect, customize as needed",
+//                     expected.trim(), stuOut.trim());
+
+// }
