d6f11943a0718310767ad768bb84c2b2b8be348d
ForShovelry1 <lguiboux@purdue.edu>
2024-10-31 16:27:29 -0400

    Started TestUserProfileInterface (testcase)

---
+++ TestUserProfileInterface.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestUserProfileInterface {
+
+    @org.junit.jupiter.api.Test
+    void getUsername() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void setUsername() {
+
+    }
+}
