9f62cc524b8cc6c7be9d27fdbbe7a9e06fadf4fc
imkim <deshpank@purdue.edu>
2024-11-02 17:02:03 -0400

    adding profileinterface test

---
+++ ProfileInterfaceTest.java
@@ -0,0 +1,107 @@
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.rules.Timeout;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import javax.swing.*;
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+/**
+ * A framework to run public test cases for the Messages class.
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2024</p>
+ *
+ * @author Purdue CS
+ * @version November 2, 2024
+ */
+
+
+public class ProfileInterfaceTest {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestCase.class);
+        if (result.wasSuccessful()) {
+            System.out.println("Excellent - Test ran successfully");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+
+    }
+
+    //certain tests possible for the messages code
+    public static class TestCase {
+        private final PrintStream originalOutput = System.out;
+        private final InputStream originalSysin = System.in;
+
+        @SuppressWarnings("FieldCanBeLocal")
+        private ByteArrayInputStream testIn;
+
+        @SuppressWarnings("FieldCanBeLocal")
+        private ByteArrayOutputStream testOut;
+
+        @Before
+        public void outputStart() {
+            testOut = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(testOut));
+        }
+
+        @After
+        public void restoreInputAndOutput() {
+            System.setIn(originalSysin);
+            System.setOut(originalOutput);
+        }
+
+        private String getOutput() {
+            return testOut.toString();
+        }
+
+        @SuppressWarnings("SameParameterValue")
+        private void receiveInput(String str) {
+            testIn = new ByteArrayInputStream(str.getBytes());
+            System.setIn(testIn);
+        }
+
+
+        @Test(timeout = 1000)
+        public void classDeclarationTestOne() {
+            Class<?> clazz;
+            int modifiers;
+            Class<?> superclass;
+            Class<?>[] superinterfaces;
+
+            // Set the class being tested
+            clazz = Profile.class;
+
+            // Perform tests
+
+            modifiers = clazz.getModifiers();
+
+            superclass = clazz.getSuperclass();
+
+            superinterfaces = clazz.getInterfaces();
+
+            Assert.assertTrue("Ensure that 'Profile' is public", Modifier.isPublic(modifiers));
+
+            Assert.assertFalse("Ensure that `Profile` is NOT `abstract`!", Modifier.isAbstract(modifiers));
+
+            Assert.assertEquals("Ensure that `Profile` extends `Object'!", Object.class, superclass);
+
+            Assert.assertEquals("Ensure that `Profile` implements 'Profile interface!", superinterfaces.length);
+        }
+
+
+
+    }
+}
