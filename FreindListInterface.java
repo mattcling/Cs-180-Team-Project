cc0a05b130da75240fce043628b6cf21f2c41379
mattcling <clinger0@purdue.edu>
2024-11-02 15:06:52 -0400

    fix

--- FreindListInterface.java
+++
@@ -1,9 +0,0 @@
-public interface FreindListInterface {
-
-    boolean SendFR(String senderID, String ReceicerID);
-    boolean AccpetFR(String senderID, String ReceicerID);
-    boolean RemoveFreind(String removedID, String userID);
-    boolean blockFreind(String blockedID, String userID);
-    boolean unblockFreind(String unblockedID, String userID);
-
-}
