f62ae630ca3e1157c2c9fa042fc75c19e5af7edb
Charlotte Falus <cfalus@purdue.edu>
2024-11-02 16:39:30 -0400

    stuff

--- MessageInterface.java
+++ MessageInterface.java
@@ -1,16 +1,16 @@
 import java.time.LocalDateTime;
 
 public interface MessageInterface {
-
     String getSenderID();
     String getReceiverID();
     String getMessageID();
-    void setMessageInfo(String message);
+    LocalDateTime getDateTime();
+
+    public void setMessageInfo(String message);
     public void setMessageID(String messageID);
     public void setSenderID(String senderID);
     public void setReceiverID(String receiverID);
     public String getMessageInfo();
-    public LocalDateTime getDateTime();
     public void setDateTime(LocalDateTime dateTime);
 
 }
